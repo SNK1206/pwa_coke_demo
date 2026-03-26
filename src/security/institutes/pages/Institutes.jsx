7ef8d2a8e4319bf252d7f54c2cacc725f038f45a
IrAntGV <bastalordbreaker@gmail.com>
2026-03-26 13:53:22 -0700

    Inclusion de las TabPages

--- src/security/institutes/pages/Institutes.jsx
+++ src/security/institutes/pages/Institutes.jsx
@@ -1,7 +1,38 @@
-export default function Institutes() {
+import { Box } from "@mui/material";
+import { useState } from "react";
+import InstitutesNavTab from "../components/tabs/InstitutesNavTab";
+import InstitutesTab from "../components/tabs/InstitutesTab";
+
+const Institutes = () => {
+
+    //FIC: indicamos que al iniciar no hay ningun Instituto seleccionado.
+    const [currentRowInInstitutesTab, setCurrentRowInInstitutesTab] = useState(0);
+    
+    //FIC: indicamos que el estado inicial del tab page principal por default sera INSTITUTOS.
+    const [currentTabInPrincipalTab, setCurrentTabInPrincipalTab] = useState("INSTITUTOS");
+    
+    //const InstitutosAllData = useSelector((state) => state.institutesReducer);
     return (
-        <div>
-            <h2>Institutes Catalog - Business </h2>
-        </div>
+        <Box>
+
+            {/* FIC: llamada intrinsica (props) */}
+
+            <InstitutesNavTab
+                setCurrentRowInInstitutesTab={setCurrentRowInInstitutesTab} 
+                setCurrentTabInPrincipalTab={setCurrentTabInPrincipalTab} 
+                setBusinessTabInPrincipalTabIsSelected={setBusinessTabInPrincipalTabIsSelected} 
+            />
+            
+            {/* FIC: si en el tap principal esta seleccionado es el tab de INSTITUTOS
+            manda llamar la pagina que va dentro del tab de Institutos. */}
+            {currentTabInPrincipalTab == "INSTITUTOS" && <InstitutesTab />}
+
+            {/* FIC: si en el tap principal esta seleccionado el tab de NEGOCIOS
+            manda llamar la pagina que va dentro del tab de Business. */}
+            {currentTabInPrincipalTab == "NEGOCIOS" && <BusinessTab />}    
+
+        </Box>
     );
-}
+};
+
+export default Institutes;
