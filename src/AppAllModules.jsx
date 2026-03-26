40303d7f95860fff069be6bba0019cbc8e136018
IrAntGV <bastalordbreaker@gmail.com>
2026-03-25 17:38:25 -0700

    Estructura Redux

--- src/AppAllModules.jsx
+++ src/AppAllModules.jsx
@@ -1,19 +1,34 @@
 import { RouterProvider } from "react-router-dom";
-//import  SecurityRouter from "./navigation/NaviRoutesSecurity";
-//import  EducationRouter from "./navigation/NaviRoutesEducation";
-import  CommerceRouter from "./navigation/NaviRoutesCommerce";
-import Footer from "./share/footer/components/Footer";
+import  Router from "./navigation/NaviRoutesEducation";
+import Footer from "./share/pages/Footer";
+//FIC: import for redux
+import { GET_DATA_START } from "./security/redux/thunks";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
 
 export default function AppAllModules() {
+	//FIC: objeto para heredar clase de dispath de redux.
+	const dispatch = useDispatch();
+	//FIC: para que solo entre la primera vez a cargar la data.
+	useEffect(() => {
+			dispatch(GET_DATA_START()).then(() => {
+				//setLoading(false);
+				console.log("<<END-DISPATCH>>: GET_DATA_START se ejecuto de forma correcta");
+			});
+	}, []);
+	
     return (
-        <>
-            <div id='div-app'>
-                {/* <h1>Main App - All Modules</h1> */}
-                <RouterProvider router={CommerceRouter} />
-                <div id='div-footer'>
-                    <Footer />
-                </div>
-            </div>
-        </> 
-    );
+		<>
+			<div id='div-app'>
+				{/* <h1>Main App - All Modules</h1> */}
+				{/* <div id='div-appbar'>
+					<AppBar />
+				</div> */}
+				<RouterProvider router={Router} />
+				<div id='div-footer'>
+					<Footer />
+				</div>
+			</div>
+		</>	
+	);
 }
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,17 @@ import ReactDOM from 'react-dom/client'
 import './share/css/allPages.css'
 //FIC: Add
 import AppAllModules from './AppAllModules';
-//import AppDemo from './AppDemo';
+import AppDemo from './AppDemo';
+//FIC: For Redux
+import { Provider } from "react-redux";
+import store from '../src/security/redux/store/store';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <App /> */}
-    <AppAllModules />
-    {/*  <AppDemo />  */}
+    <Provider store={store}>
+      <AppAllModules />
+      {/*  <AppDemo />  */}
+    </Provider>
   </React.StrictMode>,
-)
+);
